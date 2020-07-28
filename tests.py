0904cc3ea964aed7a3d75b3034ae9c757ff2b339
Marissa Aguilera <marissaguilera@yahoo.com>
2020-07-27 17:43:56 -0700

    Yes RSVP fixed

--- tests.py
+++ tests.py
@@ -13,17 +13,20 @@ class PartyTests(unittest.TestCase):
         self.client = server.app.test_client()
         server.app.config['TESTING'] = True
 
+
     def test_homepage(self):
         """Can we reach the homepage?"""
 
         result = self.client.get('/')
         self.assertIn(b'having a party', result.data)
 
+
     def test_no_rsvp_yet(self):
         """Do users who haven't RSVPed see the correct view?"""
 
         result = self.client.get('/')
         self.assertIn(b'Please RSVP', result.data)
+
 
     def test_rsvp(self):
         """Do RSVPed users see the correct view?"""
@@ -33,8 +36,11 @@ class PartyTests(unittest.TestCase):
         result = self.client.post('/rsvp', data=rsvp_info,
                                   follow_redirects=True)
 
-        # FIXME: check that once we log in we see party details--but not the form!
-        print('FIXME')
+        self.assertIn(b'Yay!', result.data)
+        self.assertIn(b'Party Details', result.data)
+        self.assertNotIn(b'Please RSVP', result.data)
+
+
 
     def test_rsvp_mel(self):
         """Can we keep Mel out?"""
